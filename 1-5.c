eda1036ad5bf563b51c44df92fcae852f46d4d0f
qingcai.zhang <zhangqingcai0815@gmail.com>
2013-04-16 15:11:44 +0800

    fork process, and run sub process.

---
+++ 1-5.c
@@ -0,0 +1,26 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    char buf[4096];
+    pid_t pid;
+    int status;
+
+    printf("%% ");
+    while(fgets(buf, MAXLINE, stdin) != NULL){
+	if(buf[strlen(buf) -1] == '\n')
+	    buf[strlen(buf) -1] = 0;
+	if((pid = fork())<0){
+	    printf("fork error.");
+	}else if(pid == 0){
+	    execlp(buf, buf, (char *)0);
+	    err_ret("could not execute: %s", buf);
+	    exit(127);
+	}
+	if((pid = waitpid(pid, &status, 0))< 0)
+	    err_sys("waitpid error.");
+	printf("%% ");
+    }
+    exit(0);
+}
